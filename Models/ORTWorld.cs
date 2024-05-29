fdd85d564b3af002ab023071c628aa40a39b7d3a
iachuDyner <48313765@est-ort.edu.ar>
2024-05-29 08:57:09 -0300

    Precargamos los datos en ORTWorld

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -1,7 +1,62 @@
-public static class ORTWorld{
+public class ORTWorld{
 public static List<string> ListaDestinos {get; private set;}
 public static List<string> ListaHoteles {get; private set;}
 public static List<string> ListaAereos {get; private set;}
 public static List<string> ListaExcursiones {get; private set;}
 public static Dictionary<string, Paquete> Paquetes {get; private set;}
+
+public ORTWorld()
+{
+    ListaDestinos[0] = "Argentina";
+    ListaDestinos[1] = "España" ;
+    ListaDestinos[2] = "Mexico";
+    ListaDestinos[3] = "Camerun";
+    ListaDestinos[4] = "Paises Bajos";
+    ListaDestinos[5] = "Portugal";
+    ListaDestinos[6] = "Australia";
+    ListaDestinos[7] = "Colombia";
+    ListaDestinos[8] = "Grecia";
+    ListaDestinos[9] = "Japon";
+
+    ListaHoteles[0] = "Hotel1.jpg";
+    ListaHoteles[1] = "Hotel2.jpg";
+    ListaHoteles[2] = "Hotel3.jpg";
+    ListaHoteles[3] = "Hotel4.jpg";
+    ListaHoteles[4] = "Hotel5.jpg";
+    ListaHoteles[5] = "Hotel6.jpg";
+    ListaHoteles[6] = "Hotel7.jpg";
+    ListaHoteles[7] = "Hotel8.jpg";
+    ListaHoteles[8] = "Hotel9.jpg";
+    ListaHoteles[9] = "Hotel10.jpg";
+
+    ListaAereos[0] = "Aereolineas1.png";
+    ListaAereos[1] = "Aereolineas2.png";
+    ListaAereos[2] = "Aereolineas3.png";
+    ListaAereos[3] = "Aereolineas4.png";
+    ListaAereos[4] = "Aereolineas5.png";
+    ListaAereos[5] = "Aereolineas6.png";
+    ListaAereos[6] = "Aereolineas7.png";
+    ListaAereos[7] = "Aereolineas8.png";
+    ListaAereos[8] = "Aereolineas9.png";
+    ListaAereos[9] = "Aereolineas10.png";
+
+    ListaExcursiones[0] = "exurciones1.jfif";
+    ListaExcursiones[1] = "exurciones2.jfif";
+    ListaExcursiones[2] = "exurciones3.jfif";
+    ListaExcursiones[3] = "exurciones4.jfif";
+    ListaExcursiones[4] = "exurciones5.jfif";
+    ListaExcursiones[5] = "exurciones6.jfif";
+    ListaExcursiones[6] = "exurciones7.jfif";
+    ListaExcursiones[7] = "exurciones8.jfif";
+    ListaExcursiones[8] = "exurciones9.jfif";
+    ListaExcursiones[9] = "exurciones10.jfif";
+
+
+
+
+}
+
+
+    
+
 }
