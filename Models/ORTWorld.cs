daa9800983a8839551557db37cf1e774ce7626b9
iachuDyner <48313765@est-ort.edu.ar>
2024-05-29 08:20:33 -0300

    creamos la clase ORTWorld

---
+++ Models/ORTWorld.cs
@@ -0,0 +1,7 @@
+public static class ORTWorld{
+public static List<string> ListaDestinos {get; private set;}
+public static List<string> ListaHoteles {get; private set;}
+public static List<string> ListaAereos {get; private set;}
+public static List<string> ListaExcursiones {get; private set;}
+public static Dictionary<string, Paquete> Paquetes {get; private set;}
+}
